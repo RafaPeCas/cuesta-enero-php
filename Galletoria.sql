b38b617ec628d5cabd3e1a8d37fd2bd55fbd96c3
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2024-01-10 19:46:58 +0100

    GalletoriaDB

---
+++ Galletoria.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS galletoria;
+CREATE DATABASE galletoria;
+USE galletoria;
+
+CREATE TABLE usuarios (
+id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nick VARCHAR(40) NOT NULL,
+cantidadGalletas INT NOT NULL DEFAULT(0),
+cpc INT NOT NULL DEFAULT(1),
+galletoriers INT NOT NULL DEFAULT(0)
+);
+
+INSERT INTO usuarios (nick, cantidadGalletas) VALUES ("guga", 150);
