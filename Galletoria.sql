44ff16c255b0134604e8fe7812ce12b54f142ea5
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2024-01-11 14:27:51 +0100

    bbdd

--- Galletoria.sql
+++ Galletoria.sql
@@ -7,7 +7,9 @@ id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nick VARCHAR(40) NOT NULL,
 cantidadGalletas INT NOT NULL DEFAULT(0),
 cpc INT NOT NULL DEFAULT(1),
-galletoriers INT NOT NULL DEFAULT(0)
+galletoriers INT NOT NULL DEFAULT(0),
+ultimaCon DATETIME NOT NULL,
+galletoriersAfk decimal NOT NULL DEFAULT(0)
 );
 
-INSERT INTO usuarios (nick, cantidadGalletas) VALUES ("guga", 150);
+INSERT INTO usuarios (nick, cantidadGalletas, ultimaCon) VALUES ("guga", 150, now());
